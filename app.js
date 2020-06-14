0a02d25193c5aaa61359cd4a3fbabcacca7ff558
arsal-farooq <64702936+arsal-farooq@users.noreply.github.com>
2020-06-14 18:20:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,772 @@
+                                                                           
+                            // ========================  CHAPTER 1  ====================
+
+
+
+
+             // ====TASK1 =====/// 
+
+
+            // alert("hellow world");
+        
+
+            
+            // ========TASK  2 ========//
+
+
+
+            // alert("Hello! Please enter the valid password");//
+
+
+
+
+            // ======TASK 3====//
+
+            // alert (" Welcome to Js land.. <br>Happy Coding !")
+
+
+
+             
+
+
+
+
+
+            // =========  TASK 5 ==========/
+
+
+          //  console.log("Hello...i can run JS through web browser console")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                            // ======================  CHAPTER 2 =========================//
+
+
+
+
+
+
+
+     
+        //===========TASK 1=======//
+
+        //  var username=""
+
+         //============TASK 2==========//
+
+
+        //  var my name = "Muhammad Arsal Farooq"
+        
+
+        //========TASK 3=============//
+
+        // var message="hello world"
+
+        // alert(message)
+
+
+
+        //===========TASK  4//
+
+        // var name= "John Doe"
+        // alert(name)
+        
+
+        // var age=" I am 15 years old"
+        // alert (age)
+
+        // var occupation= " Certified Mobile Application And Development"
+        // alert(occupation)
+
+
+       //=========tASK 5=========//
+
+      //  var  food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+      //  alert( food)
+
+
+       //========TASK 6=====//
+
+        // var email="My email address is arsal.farooq879@hotmail.com"
+        // alert (email)
+
+
+
+      //========== TASK 7======//
+      // var book ="A smarter way to learn JavaScript"
+      // alert (book)
+
+
+
+      //=================TASK 8===//
+
+      // var content ="Yah! I can write HTML through Javascript"
+      // document.write(content)
+
+
+
+
+
+
+
+
+
+
+
+      //========= ======= ================================= Chapter 3 ====================================================================//
+
+
+
+
+
+
+
+
+
+
+
+      //================TASK 1=================//
+
+
+//  var age= "I am 23 years old"
+ 
+//  alert (age)
+
+
+
+// ============TASK 3 ===============//
+
+
+
+// var birthyear =" My  birth  year is 1998   <br> Data type of my declared variable is number"
+// document.write(birthyear)
+
+
+
+
+
+// ===========TASK 3 ============//
+
+
+// var visitors name="John doe"
+// var product title ="T-shirts"
+// var quantity =  5 
+
+// var message =" <b> John Doe </b> ordered  <b>5 T-shirt(s)</b> on XYZ Clothing store "
+// document.write(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ================================================================CHAPTER 4========================================================//
+
+
+
+
+
+
+
+//==================TASK 1 ==========//
+
+// var heading= "<b> Rules for naming JS variables</b> <br> <br>"
+// document.write(heading)
+
+// var a ="  Variable names can only contain ______, ______,______ and ______. For example  <b>$my_1stVariable</b> <br> "
+// document.write (a)
+
+// var b = "Variables must begin with a ______, ______ or _____. For example <b> $name, _name or name </b>"
+// document.write(b)
+
+// var c = "Variable names are case _________ <br>"
+// document.write(c)
+
+// var d = "Variable names should not be JS _________ "
+// document.write(d)
+
+
+
+
+
+
+
+
+
+
+//================= ===================================       CHAPTER 5  ===========================================================//
+
+
+
+
+
+// ====TASK 1 ============//
+
+// var a = +prompt ("ENTER  VAlUE for addition")
+// var b = +prompt  ("ENTER ANOTHER")
+//  var c =  a+b
+//  document.write("sum of " , a, " " + "&" , " " + b,  " " +"is", " " + c)
+
+
+
+
+
+
+
+// ======TASK 2 =====//
+
+
+
+
+
+// var d = +prompt ("ENTER  Value for substraction")
+// var e = +prompt  ("ENTER ANOTHER")
+//  var f =  d-e
+//  document.write("subtraction of " , d, " " + "&" , " " + e,  " " +"is", " " + f)
+
+
+//  var g = +prompt ("ENTER VAlUE for divison")
+//  var h = +prompt  ("ENTER ANOTHER")
+//   var i =  g/h
+//   document.write("divide of " , g, " " + "&" , " " + h,  " " +"is", " " + i)
+
+
+//   var j = +prompt ("ENTER VAlUE for modulus ")
+// var k = +prompt  ("ENTER ANOTHER")
+//  var l =  j%k
+//  document.write("Modulus of " , j, " " + "&" , " " + k,  " " +"is", " " + l)
+
+
+
+
+
+
+
+
+
+
+
+// =======TASK 3 ================//
+
+// var heading= "Value after variable declaration is undefined <br>";
+// document.write(heading);
+
+// var a= 5;
+// var intial="intial value is 5 <br>";
+// document.write(intial);
+
+// var b= ++a
+// document.write("value after increment is ", " "+ a, "<br>" )
+
+// var c= b+7
+// document.write("value after adding 7 is"," " + c, "<br>")
+
+// var d= --c
+// document.write("value after decrement is  " , " "  + c, "<br>" )
+
+
+// var e= c%3
+// document.write ("The remainder is ", " " + e)
+
+
+
+
+
+
+
+
+
+
+// =======================TASK 4 =======================//
+
+// var ticket= 600
+
+// var buying = ticket * 5
+
+// document.write("Total cost to buy 5 tickets to a movie is ", " " + buying, " PKR ")
+
+
+
+
+
+
+
+
+
+
+// ========================TASK 6 ===============//
+
+
+// var celsius =   prompt("ENTER VALUE IN DEGREE")
+// var Fahrenheit= (celsius * 9/5) + 32
+// document.write(celsius, " \u00B0 C is " + Fahrenheit, " ", "\u00B0 F")
+
+
+
+// var Fahrenheit = prompt("ENTER VALUE IN FAHRENHEIT")
+
+// var celsius = ( Fahrenheit - 32 ) *5/9
+
+// document.write( Fahrenheit, "\u00B0 F is " + celsius, " ", " \u00B0 C" )
+
+
+
+
+
+
+
+
+// ================TASK 7 =======//
+
+
+
+
+// document.write( "<b> SHOPPING CART </b> <br> <br>")
+
+
+//  var item1   = 650
+
+//  document.write("Price of  item 1 is ", " " + item1   , "<br>" )
+
+//  var quantity1 =   prompt("enter quantity item 1")
+
+//   document.write (" Quantity  of item  1 is ", quantity1, " <br> " )
+
+//   var item2 = 100
+
+//   document.write( "Price of item 2 is  ", " " + item2, "<br>")
+
+//   var quantity2 =  prompt("enter quantity of item 2")
+//   document.write(" Quantity of item 2 is  ", " " + quantity2, " <br> ")
+
+//   var shipping  = 100
+//  document.write(" Shipping charges is " , " " +shipping, " <br>" )
+
+//  var totalcost = item1*quantity1 + item2*quantity2 + shipping
+
+//  document.write(" Total cost is   ", " " + totalcost)
+
+
+
+
+
+
+// =============== TASK 12 =======//
+
+
+
+
+
+
+// document.write("<b> THE  GEOMETRIZER </b> <br>")
+
+
+//  var radius = 20;
+
+//  document.write ("the radius of circle is ", " " + radius, "<br>")
+
+//   var circumference=  2 * 3.142 * radius ;
+
+//  document.write (" the circumference is " , " " + circumference, " <br>")
+
+//   var area = 3.142 * radius^2
+
+//   document.write("The area is " , " "+ area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ===============TASK 11 =======//
+
+
+
+// var currentyear = prompt(" ENTER CURRENT YEAR ")
+// var birthyear = prompt(" ENTER YOUR BIRTH YEAR")
+// var age = currentyear - birthyear
+// document.write( " Current year : ",  " " + currentyear, " <br>", "Your birth year:", " "+birthyear, "<br>", "Your age  is ", " "+age )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================================================== CHAPTER 6 TO 9 ========================================================//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==============TASK 1 ===========/
+
+
+// var a = prompt("enter your value ")
+// document.write("the value of a is", " "+ a , " <br><br><br> <br>")
+
+
+// var b = ++a 
+
+// document.write("the value  of ++a is", " " +b, " <br> " )
+
+// document.write(" now the value of a is", " "+ b, " <br> <br>")
+
+
+// var c =  b++
+// document.write("  the value of a++", " "+ c, " <br> ") 
+// var d=  b
+// document.write(" now the  value of  a is  ", " " +d ," <br><br>")
+
+
+
+// var e = --d
+// document.write("the value of --a is "," "+d, " <br>")
+// document.write("now the value of a is ", " " +d, "<br><br>")
+
+
+// var f= e--
+// document.write(" the value of a--", " "+f, "<br>")
+// var f = e
+// document.write("now the value of a is "," "+f)
+
+
+
+
+
+
+
+// =========  TASK 2 =============//
+
+// var a = 2 ;
+// var b = 1 ;
+// document.write(" the  value of a is", " "+a, " <br>", " the value  of  b is ", " " + b , " <br>")
+
+
+// var result = --a - --b + ++b + b--
+
+// document.write (" the output is ", " " +result)
+
+
+
+
+
+
+  //=====================TASK  3 ==============//
+
+
+
+
+//   var  name = prompt(" Enter your name ")
+
+// alert ("Hello " + name + "! Welcome to our website " )
+
+
+
+
+
+
+
+
+
+
+// =================TASK 6 ==========//
+
+
+// var subject1 = prompt("Enter first subject name")
+// var subject2 = prompt("Enter second subject name")
+// var subject3 = prompt("Enter third subject name")
+
+// var total = 100;
+
+
+// var obtained1 =  prompt("Enter "+ subject1 + "Marks")
+// var obtained2 =  prompt("Enter "+ subject2 + "Marks")
+// var obtained3 =  prompt("Enter "+ subject3 + "Marks")
+
+
+// var totalmarks = total + total + total
+
+
+// var totalobtained = obtained1 + obtained2  + obtained3 
+
+
+
+// var totalpercent =   totalobtained / totalmarks * 100 ;
+
+
+
+
+
+
+
+
+
+// =============================================== CHAPTER 9 TO 11=======================================================================//
+
+
+
+// =======  TASK 1 =============//
+
+// var city = prompt("enter your  city");
+
+
+// if (  city ==  "karachi"){
+//     alert(" Welcome to city of lights")
+// }
+
+
+
+
+
+
+
+
+
+// =========== TASK 2 =====//
+
+
+
+// var gender = prompt(" Your gender")
+ 
+// if( gender== "male"){
+//     alert("Good Morning Sir")
+
+// }
+
+// else{
+//    alert("Good Morning Ma'am")
+// }
+
+
+
+
+
+
+
+
+
+// ==================== TASK 3 ===============//
+
+
+
+  //  var signal =  prompt("  Enter signal color ")
+
+  //  if ( signal === " red"){
+  //  alert ("Must Stop ")
+  //  } if  ( signal === " yellow "){
+  //    alert("Ready to Move")
+
+  //   }if (signal === " green"){
+  //     alert("Move Now")
+  //  }
+
+
+
+
+
+
+// ================ TASK 4 ========//
+
+// var fuel  = prompt("Remaining fuel in your  car (in litres)")
+
+//  if( fuel < 0.25){
+//      alert("Please refill the fuel in your car")
+//  }
+
+
+
+
+
+
+
+
+// ================TASK 10================ //
+
+
+// var temp = prompt(" Enter Temperature ")
+
+// if ( temp > 40){
+//     alert("It is too hot outside.")
+// }
+
+// if ( temp > 30){
+//     alert("The Weather today is Normal.")
+// }
+
+// if (temp > 20) {
+//     alert(" Today’s Weather is cool.")
+// }
+
+// if( temp> 10){
+//     alert(" OMG! Today’s weather is so Cool")
+// }
+
+
+
+
+
+
+
+// =============================================================== CHAPTER 14 TO 16 ===================================================//
+
+
+
+
+
+// ==================== TASK 1 ============//
+
+
+// var students = [];
+
+
+
+
+
+// =============== TASK 2 ===========//
+
+
+// var s = [" arsal"];
+
+
+
+
+
+
+//  ============== TASK 3 =======///
+
+// var n = [2]
+
+
+
+
+
+
+
+//  =================== TASK 6 ============ ////
+
+
+
+
+// var m =[ " arsal", " mixed", 123]
+
+
+
+
+
+// ================ TASK 7 ==============/
+
+
+
+// var education = [ "SSC", "HSC", "BCS"]
+
+// document.write (" <h1> Qualificatio</h1> <br>")
+
+// document.write (" " + education[0], " <br> " + education[1], " " + education[2])
+
+
+
+
+
+
+// ============== TASK 15 =================//
+
+
+
+// var arr = ["Apple ", " Motorolla ", " samsung ", " nokia ", "Sony & haier "]
+
+// document.write("<select>")
+// for (var i= 0; i < arr.length; i++){
+//   document.write  ("<option>" + arr[i] + "</option>")
+// }
+
+// document.write("</select>")
+
+
+
+
+
+
+
+
+
+
+
+// ===================================================== CHAPTER 17 TO 20 ==================================================//
+
+
+
+
+// =============  TASK 7 ================//
+
+
+
+
+
+//  var  items = ["cake","apple pie","cookie","chips","patties"]
+
+
+//  var search = prompt("hello to xyz bakery , what  do you want to order sir/ maam ")
+
+
+
+// if (items.indexOf(search)!== -1){
+//   alert(  "yes "+ search + " is available ");
+
+//  } else {
+//    alert ("no " + search +" is not available ");
+//  }
+
+
+
+
+
